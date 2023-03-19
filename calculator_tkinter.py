2186a0e048e826e4166a6db420c32b8a73180326
mariusciurea <ciurea.marius1@gmail.com>
2023-03-19 12:00:49 +0200

    calculator code updated

--- calculator_tkinter.py
+++ calculator_tkinter.py
@@ -2,50 +2,44 @@ import tkinter as tk
 
 
 class Calculator:
+
     expression = ''
 
     def __init__(self):
         self.root = tk.Tk()
-        self.root.title('Calculator')
-        self.root.geometry('300x390')
-        self.root.resizable(0, 0)
-        self.root.config(bg='#45458B')
+        self.root.geometry('300x330')
+        self.root.resizable(False, False)
+        self.root.configure(bg="#2399C9")
+
+        self.frame = tk.Frame(self.root, width=300, height=400,
+                              bg='#2399C9', highlightcolor='black')
+        self.frame.grid(row=0, column=0)
+        self.frame.grid_propagate(False)
 
         self.str_var = tk.StringVar()
-
-        self.frame = tk.Frame(self.root, width=500,
-                              height=100,
-                              bg='#45458B',
-                              highlightcolor='black')
-        self.frame.pack()
-
-        self.text = tk.Entry(self.frame, width=30, bg='#83838B',
+        self.text = tk.Entry(self.frame, width=24, bg='#525759',
                              font=('Arial', 16, 'bold'),
-                             textvariable=self.str_var,
-                             justify=tk.RIGHT)
-        self.text.pack(padx=10, pady=10, fill='both')
+                             fg='white', justify=tk.RIGHT,
+                             textvariable=self.str_var)
+        self.text.grid(row=0, column=0, padx=2, pady=10, sticky=tk.E + tk.W)
 
-        self.clear_button = tk.Button(self.frame,
-                                      text='C',
-                                      font=('Arial', 16),
-                                      bg='#7F7FFF',
-                                      fg='white',
-                                      activebackground='#8B8B83',
-                                      command=self.clear_text
-                                      )
-        self.clear_button.pack(padx=5, pady=5, fill='both')
+        self.clear = tk.Button(
+            self.frame,
+            text='C',
+            font=('Arial', 16, 'bold'),
+            bg='#044A66',
+            fg='white',
+            activebackground='gray',
+            activeforeground='blue',
+            command=self.clear_text
+        )
+        self.clear.grid(row=1, column=0, padx=2, pady=5, sticky=tk.E + tk.W)
 
-        self.frame.pack_propagate(False)
-
-        self.frame_numbers = tk.Frame(self.root,
-                                      bg='black',
-                                      highlightcolor='black')
-        self.frame_numbers.pack(padx=5, pady=5)
+        self.button_frame = tk.Frame(self.frame, width=300, height=400,
+                              bg='#2399C9', highlightcolor='black')
+        self.button_frame.grid(row=2, column=0)
 
         self.numbers_grid()
-
-        self.frame_numbers.pack_propagate(False)
-
         self.root.mainloop()
 
     def clear_text(self):
@@ -55,72 +49,32 @@ class Calculator:
     def btn_click(self, strng):
         if strng == '=':
             Calculator.expression = str(eval(Calculator.expression))
-            self.str_var.set(str(Calculator.expression))
+            self.str_var.set(Calculator.expression)
         else:
             Calculator.expression += strng
             self.str_var.set(Calculator.expression)
 
     def add_numbers(self, btn_number):
-        self.btn = tk.Button(self.frame_numbers,
+        self.btn = tk.Button(self.button_frame,
                              text=btn_number,
                              width=5,
-                             height=2,
+                             height=1,
                              font=('Arial', 16),
-                             bg='#7F7FFF',
+                             bg='#044A66',
                              fg='white',
-                             activebackground='#8B8B83',
+                             activebackground='gray',
+                             activeforeground='blue',
                              command=lambda: self.btn_click(btn_number)
-                             )
+                            )
         return self.btn
 
     def numbers_grid(self):
-        btn = self.add_numbers('7')
-        btn.grid(row=0, column=0, padx=1, pady=1)
-
-        btn = self.add_numbers('8')
-        btn.grid(row=0, column=1, padx=1, pady=1)
-
-        btn = self.add_numbers('9')
-        btn.grid(row=0, column=2, padx=1, pady=1)
-
-        btn = self.add_numbers('*')
-        btn.grid(row=0, column=3, padx=1, pady=1)
-
-        btn = self.add_numbers('4')
-        btn.grid(row=1, column=0, padx=1, pady=1)
-
-        btn = self.add_numbers('5')
-        btn.grid(row=1, column=1, padx=1, pady=1)
-
-        btn = self.add_numbers('6')
-        btn.grid(row=1, column=2, padx=1, pady=1)
-
-        btn = self.add_numbers('-')
-        btn.grid(row=1, column=3, padx=1, pady=1)
-
-        btn = self.add_numbers('1')
-        btn.grid(row=2, column=0, padx=1, pady=1)
-
-        btn = self.add_numbers('2')
-        btn.grid(row=2, column=1, padx=1, pady=1)
-
-        btn = self.add_numbers('3')
-        btn.grid(row=2, column=2, padx=1, pady=1)
-
-        btn = self.add_numbers('+')
-        btn.grid(row=2, column=3, padx=1, pady=1)
-
-        btn = self.add_numbers('+/-')
-        btn.grid(row=3, column=0, padx=1, pady=1)
-
-        btn = self.add_numbers('0')
-        btn.grid(row=3, column=1, padx=1, pady=1)
-
-        btn = self.add_numbers('/')
-        btn.grid(row=3, column=2, padx=1, pady=1)
-
-        btn = self.add_numbers('=')
-        btn.grid(row=3, column=3, padx=1, pady=1)
+        numbers = [('7', '8', '9', '*'), ('4', '5', '6', '-'), ('4', '5', '6', '-'), ('1', '2', '3', '+'),
+                   ('%', '0', '/', '=')]
+        for i, item in enumerate(numbers):
+            for j, number in enumerate(item):
+                btn = self.add_numbers(number)
+                btn.grid(row=i, column=j, padx=1, pady=1)
 
 
 if __name__ == '__main__':
